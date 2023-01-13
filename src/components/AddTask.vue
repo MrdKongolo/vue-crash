f67fa92f7b88955e39a284e0d7b39b2764295f24
MrdKongolo <medardbaraka09@gmail.com>
2023-01-13 15:56:59 +0200

    first commit

---
+++ src/components/AddTask.vue
@@ -0,0 +1,92 @@
+<template>
+    <form @submit="onSubmit" class="add-form">
+        <div class="form-control">
+            <label>Tâche</label>
+            <input type="text" v-model="text" name="text" placeholder="Ajouter" />
+        </div>
+        <div class="form-control">
+            <label>Jour & Heure</label>
+            <input type="text" name="day" v-model="day" placeholder="Ajouter Jour & heure" />
+        </div>
+        <div class="form-control form-control-check">
+            <label>Se Souvenir</label>
+            <input type="checkbox" v-model="reminder" name="reminder" />
+        </div>
+
+        <input type="submit" value="Sauvegarder" class="btn btn-block" />
+    </form>
+</template>
+
+<script>
+  export default {
+    name: 'AddTask',
+    data() {
+        return {
+            text: '',
+            day: '',
+            reminder: false,
+        }
+    },
+    methods: {
+        onSubmit(e) {
+            e.preventDefault()
+
+            if (!this.text) {
+                alert('Ajouter une tâche')
+                return
+            }
+
+            const newTask = {
+                id: Math.floor(Math.random() * 100000),
+                text: this.text,
+                day: this.day,
+                reminder: this.reminder
+            }
+
+            console.log(newTask)
+
+            this.text = ''
+            this.day = ''
+            this.reminder = false
+        },
+    },
+  }
+    
+</script>
+
+<style scoped>
+  .add-form {
+      margin-bottom: 40px;
+  }
+  
+  .form-control {
+      margin: 20px 0;
+  }
+  
+  .form-control label {
+      display: block;
+  }
+  
+  .form-control input {
+      width: 100%;
+      height: 40px;
+      margin: 5px;
+      padding: 3px 7px;
+      font-size: 17px;
+  }
+  
+  .form-control-check {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+  }
+  
+  .form-control-check label {
+      flex: 1;
+  }
+  
+  .form-control-check input {
+      flex: 2;
+      height: 20px;
+  }
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,39 @@
+<template>
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>
+            {{ task.text }}  
+            <i class="fas fa-times" @click="$emit('delete-task',id)"></i>
+        </h3>
+        <p>{{ task.day }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Task',
+        props: {
+            task: Object,
+        },
+        // emits: ['toggle-reminder','delete-task']
+    }
+</script>
+
+<style scope>
+    .fas {
+        color: red;
+    }
+    .task {
+        background: #f4f4f4;
+        margin: 5px;
+        padding: 10px 20px;
+        cursor: pointer;
+    }
+    .task.reminder {
+        border-left: 5px solid green;
+    }
+    .task h3 {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
